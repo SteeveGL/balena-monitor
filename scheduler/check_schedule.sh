903b3062824db5997d4fa359eb26badf9bd1bec0
Steeve Gaudreault-Lambert <steeveglambert@gmail.com>
2026-09-11 16:03:08 +0000

    Refactor: Simplify time conversion logic in time_to_minutes

--- scheduler/check_schedule.sh
+++ scheduler/check_schedule.sh
@@ -61,10 +61,8 @@ check_schedule() {
     # Helper to convert HH:MM string to minutes past midnight
     time_to_minutes() {
         local time_str=$1
-        local h=${time_str:0:2}
-        local m=${time_str:2:2}
-        h=${h#0}
-        m=${m#0}
+        local h=$((10#${time_str:0:2}))
+        local m=$((10#${time_str:2:2}))
         echo $((h * 60 + m))
     }
 
